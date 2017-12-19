e000f1375c522a74695dc5be1188c250410659b0
chuangmaster <chuang.master@gmail.com>
2017-12-20 01:17:00 +0800

    加入模型驗證(Email)不允許重複

--- MVCHomeWork/Models/客戶聯絡人.Partial.cs
+++ MVCHomeWork/Models/客戶聯絡人.Partial.cs
@@ -5,40 +5,51 @@ namespace MVCHomeWork.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
+    using MVCHomeWork.Models;
+    using System.Linq;
+
     [MetadataType(typeof(客戶聯絡人MetaData))]
-    public partial class 客戶聯絡人
+    public partial class 客戶聯絡人 : IValidatableObject
     {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            CustomerEntities db = new CustomerEntities();
+            var email = db.客戶聯絡人.Where(x => x.Email == this.Email).FirstOrDefault();
+            if (email != null)
+            {
+                yield return new ValidationResult("Email欄位重複，請重新輸入", new string[] { "Email" });
+            }
+        }
     }
-    
+
     public partial class 客戶聯絡人MetaData
     {
         [Required]
         public int Id { get; set; }
         [Required]
         public int 客戶Id { get; set; }
-        
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         [Required]
         [JsonProperty("job")]
         public string 職稱 { get; set; }
-        
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         [Required]
         [JsonProperty("name")]
         public string 姓名 { get; set; }
-        
-        [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
+
+        [StringLength(250, ErrorMessage = "欄位長度不得大於 250 個字元")]
         [Required]
         [JsonProperty("email")]
         public string Email { get; set; }
-        
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
-        [MobilePhoneValidate(ErrorMessage ="手機格式必須如0911-111111")]
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         [JsonProperty("phone")]
+        [RegularExpression(@"09\d{2}-\d{6}", ErrorMessage = "手機格式必須如0911-111111")]
         public string 手機 { get; set; }
-        
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         [JsonProperty("tellPhone")]
         public string 電話 { get; set; }
         [JsonIgnore]
